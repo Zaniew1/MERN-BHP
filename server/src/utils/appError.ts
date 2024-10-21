dc67a5138518970f210e8d1f6e1c99a145170b9c
Mateusz <m.zaniewski1995@gmail.com>
2024-10-21 18:32:54 +0200

    remove appError

--- server/src/utils/appError.ts
+++ server/src/utils/appError.ts
@@ -1,13 +1,9 @@
+import AppErrorCode from "../constants/appErrorCode";
+import { HttpStatusCode } from "../constants/http";
+
 class AppError extends Error {
-  statusCode: number;
-  status: string;
-  isOperational: boolean;
-  constructor(message: string, statusCode: number) {
+  constructor(public statusCode: HttpStatusCode, public message: string, public errorCode?: AppErrorCode) {
     super(message);
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
-    this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
   }
 }
 
